774b33bac828cd5094cc60ebe57bbe685ad36929
Margarita Masko <margaritam996@gmail.com>
2025-02-13 21:02:31 +0300

    feat: implement POST request

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,14 @@ import db from './db.json'
 
 enum Method {
   Get = 'GET',
-  Post = 'Post',
-  Delete = 'Delete',
-  Put = 'Put',
+  Post = 'POST',
+  Delete = 'DELETE',
+  Put = 'PUT',
 }
-const requestsGet = (
-  userId: string | null,
-  method: string | undefined,
-  req: http.IncomingMessage,
-  res: http.ServerResponse,
-) => {
-  if (method === Method.Get && req.url) {
+let userIdCounter = db.users.length + 1
+
+const requestsGet = (userId: string | null, req: http.IncomingMessage, res: http.ServerResponse) => {
+  if (req.url) {
     if (req.url === '/users') {
       res.writeHead(200, { 'Content-Type': 'application/json' })
       res.end(JSON.stringify(db.users))
@@ -42,12 +39,50 @@ const requestsGet = (
   res.writeHead(404, { 'Content-Type': 'application/json' })
   res.end(JSON.stringify({ message: 'URL does not exist' }))
 }
+
+const requestsPost = (req: http.IncomingMessage, res: http.ServerResponse) => {
+  if (req.url && req.url === '/users') {
+    let body = ''
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+    req.on('end', () => {
+      const newUser = JSON.parse(body)
+
+      if (!newUser.username || !newUser.age) {
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Username and age are required.' }))
+        return
+      }
+      if (newUser.username && newUser.age) {
+        const user = {
+          id: String(userIdCounter++),
+          username: newUser.username,
+          age: newUser.age,
+          hobbies: newUser.hobbies || [],
+        }
+
+        db.users.push(user)
+
+        res.writeHead(201, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(user))
+        return
+      }
+    })
+  }
+  res.writeHead(404, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify({ message: 'URL does not exist' }))
+}
+
 const server = http.createServer((req, res) => {
-  const url = new URL(`https://localhost:300/${req.url}`)
+  const url = new URL(`https://localhost:3000/${req.url}`)
   const userId = url.searchParams.get('id')
   const method = req.method
-
-  requestsGet(userId, method, req, res)
+  if (method === Method.Get) {
+    requestsGet(userId, req, res)
+  } else if (method === Method.Post) {
+    requestsPost(req, res)
+  }
 })
 
 server.listen(3000)
